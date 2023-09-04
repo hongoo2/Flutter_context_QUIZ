1c8a9204e3c5bd729a6a786cd9cab3e896c0fad3
hongu <namhongui0312@gmail.com>
2023-09-04 21:35:21 +0900

    This is Quiz

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Center(
+        child: ElevatedButton(
+            child: Text('Go to Screen A'),
+            onPressed: () => Navigator.push(
+                context, MaterialPageRoute(builder: (context) => ScreenA()))),
+      ),
+    );
+  }
+}
+
+//MaterialApp 과 context의 위치를 잘 살펴보시기 바랍니다
+
+class ScreenA extends StatelessWidget {
+  const ScreenA({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Hi there!'),
+      ),
+    );
+  }
+}
